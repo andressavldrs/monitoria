34bd6593f29e5c9f73d28afb9b33b661a94d106d
Andressa Valadares <andressarodrial@gmail.com>
2017-07-02 19:30:57 -0300

    Alterações nas telas de "Tipo de Monitoria"
    Mudança dos textos de "Exibir", "Excluir", "Novo", "Editar" para
    imagens. Aterações dos títulos e inserção de validações.
    
    closes #10

--- app/controllers/tipos_controller.rb
+++ app/controllers/tipos_controller.rb
@@ -28,7 +28,7 @@ class TiposController < ApplicationController
 
     respond_to do |format|
       if @tipo.save
-        format.html { redirect_to @tipo, notice: 'Tipo was successfully created.' }
+        format.html { redirect_to @tipo, notice: 'Tipo de monitoria foi atualizado com sucesso.' }
         format.json { render :show, status: :created, location: @tipo }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class TiposController < ApplicationController
   def update
     respond_to do |format|
       if @tipo.update(tipo_params)
-        format.html { redirect_to @tipo, notice: 'Tipo was successfully updated.' }
+        format.html { redirect_to @tipo, notice: 'Tipo de monitoria foi atualizado com sucesso.' }
         format.json { render :show, status: :ok, location: @tipo }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class TiposController < ApplicationController
   def destroy
     @tipo.destroy
     respond_to do |format|
-      format.html { redirect_to tipos_url, notice: 'Tipo was successfully destroyed.' }
+      format.html { redirect_to tipos_url, notice: 'Tipo de monitoria foi removido com sucesso.' }
       format.json { head :no_content }
     end
   end
--- app/models/tipo.rb
+++ app/models/tipo.rb
@@ -1,3 +1,4 @@
 class Tipo < ApplicationRecord
 	has_many :candidatos
+	validates :descricao, uniqueness: { case_sensitive: false }
 end
